dccea922bf22c9c4c197d8fe6c71e49c93d97596
Afeef <contact@afeef.dev>
2024-12-08 01:21:39 +0800

    feat: reset app and added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeader } from './core/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeader],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cv-frontend';
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { InputSwitchComponent } from '../../shared/input-switch/input-switch.component';
 
 @Component({
   selector: 'app-header',
+  imports: [InputSwitchComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class AppHeader {
+  title: string = 'Afeef bin Abu Bakar';
+}
