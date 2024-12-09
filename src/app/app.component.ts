2b2b822c249058de85c25f5d0831cc08b3e243b8
Afeef <contact@afeef.dev>
2024-12-10 00:26:18 +0800

    feat: profile summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeader } from './core/header/header.component';
+import { ProfileSummaryComponent } from './features/profile/views/profile-summary.views';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeader],
+  imports: [RouterOutlet, AppHeader, ProfileSummaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/features/profile/views/profile-summary.views.ts
@@ -0,0 +1,27 @@
+import { Component, effect, inject } from '@angular/core';
+import { ProfileDataService } from '../services/profile-data.services';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-summary',
+  imports: [NgIf],
+  templateUrl: './profile-summary.views.html',
+  styleUrls: ['./profile-summary.views.css'],
+})
+export class ProfileSummaryComponent {
+  private profileDataService: ProfileDataService = inject(ProfileDataService);
+
+  profileSummary: string = '';
+
+  constructor() {
+    effect(() => {
+      const profile = this.profileDataService.profile();
+      profile?.subscribe((profile) => {
+        if (profile) {
+          this.profileSummary = profile.summary;
+          console.log(this.profileSummary);
+        }
+      });
+    });
+  }
+}
